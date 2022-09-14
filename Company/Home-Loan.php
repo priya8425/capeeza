3e4fae054bb26b84eae030ef5dae1a90d82d5b17
priya8425 <yadavpriya1425@gmail.com>
2022-09-14 16:30:56 +0530

    services pages

---
+++ Company/Home-Loan.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Contact - Company Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Company - v4.8.0
+  * Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><span>Com</span>pany</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+
+          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="team.html">Team</a></li>
+              <li><a href="testimonials.html">Testimonials</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html" class="active">Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg); ">
+          <div class="carousel-container">
+            
+              
+          </div>
+        </div>
+      </div>
+
+     
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+    </div>
+  </section>
+  <main id="main">
+
+    <section id="contact" class="contact">
+      <div class="container">
+         
+        <div class="row justify-content-center" data-aos="fade-up">
+
+          <div class="col-lg-10">
+            <h2 style="color: rgb(10, 10, 10); text-align: center;"><b>Home Loan</b></h2>
+            <hr>
+            <p>Purchasing a home is a valuable form of investment. Self owned house is a life time decision for any individual. A Home Loan is a secured loan product where the lender provides financing for the purchase or construction of a residential/commercial property. A home loan is an amount of money that an individual borrows from a financial institution at a certain rate of interest to be paid with the EMI every month. The property is taken as a security, by the financial institution.</p>
+            <br>
+            <p>Our home loan services is suitable for all types of customer base viz., salaried, self-employed, professional, NRIs, etc. At present, we are dealing with leading banks and NBFC’s that can provide best-customized deals for you with home loan products depending upon your requirement. In home loan, you don’t have to do so. Many major banks and NBFC’s grant loans up to 85% to 90% amount of the house. So you just have to pay remaining 10% or 15% amount from your savings.</p><br>
+            <h2>Tax Benefits</h2>
+            <br>
+            <p>You can save on your yearly taxes under section 24, section 80C and section 80EE under the Income tax act of India. You secure your savings: Your savings are very important for you and spending them all in one go can be a little heartbreaking.</p><br>
+            <h2>Balance Transfer</h2>
+            <br>
+            <p>Balance Transfer or Takeover of your existing home loan with other financial institute is also possible, who is offering repayment at a lower interest rate along with changes in the provisions that may not be in accordance with you. Home loan refinancing is one of the best ways to avail a lower interest rate and make savings on the home loan.</p>
+           
+          <hr>
+
+          </div>
+
+        </div>
+
+        
+
+      </div>
+    </section><!-- End Contact Section -->
+    
+    
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>Company</h3>
+            <p>
+              We are located at Pune and<br> Navi Mumbai providing a wide<br>  range of services on PAN India<br> basis and across the Globe. <br><br>
+              <strong>Phone:</strong>  +91 9371151356<br>
+              <strong>Email:</strong>info@capeezacorp.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Income Tax Department</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Central Board Of Excise & Custom</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                E-Tax Information Network</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Ministry Of Company Affairs</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Employees Provident fund</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">GST</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">RERA</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Connect With Us</h4>
+            <ul>
+              <h6>Manchar</h6>
+              <li><i class="bi bi-geo-alt"></i> <a href="#">
+                Trimurti pride, Below Bank of Maharashtra, Manchar, Pune - 410 503</a></li>
+                <h6>Pune</h6>
+                <li><i class="bi bi-geo-alt"></i> <a href="#">
+                  Kunal Icon Road, Pimple Saudagar, Pune - 411 027</a></li>
+                  <h6>Navi Mumbai</h6>
+                  <li><i class="bi bi-geo-alt"></i> <a href="#">
+                    03, Sai Villa, Plot No. 75, Sector - 20, Ulwe, Navi Mumbai - 410 206</a></li>
+                   
+                    <li><i class="bi bi-geo-alt"></i> <a href="#">
+                      Office No. 69, Thapar Complex, Plot No. 51, Sec - 15, CBD Belapur, Navi Mumbai - 400 614.</a></li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>2022 Capeeza Corp Services|</span></strong>. All Rights Reserved
+        </div>
+        
+      </div>
+      <div class="credits" style="float: right;">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+        Designed by <a href="https://tectignis.in/"> Tectignis IT Solutions.</a>
+      </div>
+    </div>
+    
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ Company/ROC-Compliance.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Contact - Company Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Company - v4.8.0
+  * Template URL: https://bootstrapmade.com/company-free-html-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><span>Com</span>pany</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html">Home</a></li>
+
+          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="team.html">Team</a></li>
+              <li><a href="testimonials.html">Testimonials</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="services.html">Services</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="pricing.html">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.html" class="active">Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg); ">
+          <div class="carousel-container">
+            
+              
+          </div>
+        </div>
+      </div>
+
+     
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+    </div>
+  </section>
+  <main id="main">
+
+    <section id="contact" class="contact">
+      <div class="container">
+         
+        <div class="row justify-content-center" data-aos="fade-up">
+
+          <div class="col-lg-10">
+            <h2 style="color: rgb(10, 10, 10); text-align: center;">ROC / ROF Compliances</h2><br>
+            <hr>
+           <br>
+            <p>Every Company is mandatorily required to comply with Registrar of Companies (ROC) by filing various online forms on the website of the ROC ( Ministry of Corporate Affairs), providing financial and other details of the company periodically. At times, this periodical filing of forms with ROC becomes a pain for Start-Ups and Small to Medium Level companies, as they are not very sure of details to be filed in these forms, selection of forms also becomes big hassle.</p>
+            <br>
+          <hr>
+
+          </div>
+
+        </div>
+
+        
+
+      </div>
+    </section><!-- End Contact Section -->
+    
+    
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>Company</h3>
+            <p>
+              We are located at Pune and<br> Navi Mumbai providing a wide<br>  range of services on PAN India<br> basis and across the Globe. <br><br>
+              <strong>Phone:</strong>  +91 9371151356<br>
+              <strong>Email:</strong>info@capeezacorp.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Income Tax Department</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Central Board Of Excise & Custom</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                E-Tax Information Network</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Ministry Of Company Affairs</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Employees Provident fund</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">GST</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">RERA</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Connect With Us</h4>
+            <ul>
+              <h6>Manchar</h6>
+              <li><i class="bi bi-geo-alt"></i> <a href="#">
+                Trimurti pride, Below Bank of Maharashtra, Manchar, Pune - 410 503</a></li>
+                <h6>Pune</h6>
+                <li><i class="bi bi-geo-alt"></i> <a href="#">
+                  Kunal Icon Road, Pimple Saudagar, Pune - 411 027</a></li>
+                  <h6>Navi Mumbai</h6>
+                  <li><i class="bi bi-geo-alt"></i> <a href="#">
+                    03, Sai Villa, Plot No. 75, Sector - 20, Ulwe, Navi Mumbai - 410 206</a></li>
+                   
+                    <li><i class="bi bi-geo-alt"></i> <a href="#">
+                      Office No. 69, Thapar Complex, Plot No. 51, Sec - 15, CBD Belapur, Navi Mumbai - 400 614.</a></li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>2022 Capeeza Corp Services|</span></strong>. All Rights Reserved
+        </div>
+        
+      </div>
+      <div class="credits" style="float: right;">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+        Designed by <a href="https://tectignis.in/"> Tectignis IT Solutions.</a>
+      </div>
+    </div>
+    
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
