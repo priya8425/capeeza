aa0768bca911111ad73dd6edf33af375305809ea
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-09-15 09:56:50 +0530

    sss

--- include/footer.php
+++ include/footer.php
@@ -1,272 +1,72 @@
-/*--------------------------------------------------------------
-# Footer
---------------------------------------------------------------*/
-#footer {
-  color: rgb(14, 13, 13);
-  font-size: 14px;
-  background: #ebebeb;
-}
-
-#footer .footer-top {
-  padding: 60px 0 30px 0;
-  background: #ebebeb;
-}
-
-#footer .footer-top .footer-contact {
-  margin-bottom: 30px;
-}
-
-#footer .footer-top .footer-contact h4 {
-  font-size: 22px;
-  margin: 0 0 30px 0;
-  padding: 2px 0 2px 0;
-  line-height: 1;
-  font-weight: 700;
-}
-
-#footer .footer-top .footer-contact p {
-  font-size: 15px;
-  line-height: 24px;
-  margin-bottom: 0;
-  font-family: "Raleway", sans-serif;
-  color: #0e0d0d;
-}
-
-#footer .footer-top h4 {
-  font-size: 16px;
-  font-weight: bold;
-  color: #0e0d0d;
-  position: relative;
-  padding-bottom: 12px;
-}
-
-#footer .footer-top .footer-links {
-  margin-bottom: 30px;
-}
-
-#footer .footer-top .footer-links ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-}
-
-#footer .footer-top .footer-links ul i {
-  padding-right: 2px;
-  color: #ff5821;
-  font-size: 18px;
-  line-height: 1;
-}
-
-#footer .footer-top .footer-links ul li {
-  padding: 10px 0;
-  display: flex;
-  align-items: center;
-}
-
-#footer .footer-top .footer-links ul li:first-child {
-  padding-top: 0;
-}
-
-#footer .footer-top .footer-links ul a {
-  color: #0e0d0d;
-  transition: 0.3s;
-  display: inline-block;
-  line-height: 1;
-}
-
-#footer .footer-top .footer-links ul a:hover {
-  text-decoration: none;
-  color: #ff5821;
-}
-
-#footer .footer-newsletter {
-  font-size: 15px;
-}
-
-#footer .footer-newsletter h4 {
-  font-size: 16px;
-  font-weight: bold;
-  color: #0e0d0d;
-  position: relative;
-  padding-bottom: 12px;
-}
-
-#footer .footer-newsletter form {
-  margin-top: 30px;
-  background: #fff;
-  padding: 6px 10px;
-  position: relative;
-  border-radius: 50px;
-  text-align: left;
-  border: 1px solid #ffe2d9;
-}
-
-#footer .footer-newsletter form input[type=email] {
-  border: 0;
-  padding: 4px 8px;
-  width: calc(100% - 100px);
-}
-
-#footer .footer-newsletter form input[type=submit] {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  border: 0;
-  background: none;
-  font-size: 16px;
-  padding: 0 20px;
-  background: #ff5821;
-  color: #fff;
-  transition: 0.3s;
-  border-radius: 50px;
-  box-shadow: 0px 2px 15px rgba(0, 0, 0, 0.1);
-}
-
-#footer .footer-newsletter form input[type=submit]:hover {
-  background: #ed3b00;
-}
-
-#footer .credits {
-  padding-top: 5px;
-  font-size: 13px;
-  color: #fff;
-}
-
-#footer .social-links a {
-  font-size: 18px;
-  display: inline-block;
-  background: #fff;
-  color: #ff4000;
-  line-height: 1;
-  padding: 8px 0;
-  margin-right: 4px;
-  border-radius: 50%;
-  text-align: center;
-  width: 36px;
-  height: 36px;
-  transition: 0.3s;
-}
-
-#footer .social-links a:hover {
-  background: #ff4000;
-  color: #fff;
-  text-decoration: none;
-}
-
-@media (max-width: 768px) {
-  .copyright_footer{
-    text-align: center !important;
-  }
-}
-
-
-
-<!-- ======= Footer ======= -->
-<footer id="footer">
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
 
     <div class="footer-top">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-lg-4 col-md-6 footer-contact">
-                    <h1><a href="index.php"><img src="assets/images/logo/logo-capeeza.webp" alt=""></a></h1>
-                    <p class="mt-3">We are located at Pune and Navi Mumbai providing a wide range of services on PAN
-                        India basis and across the Globe. <br>
-                    </p>
-                    <p class="mt-2">
-                        <b class="my-3"><i class="bi bi-telephone" style="color:#ff5821;"></i>
-                        <a style="color:#000" href="tel:+919371151356"> +91 9371151356</a></b><br>
-                        <b class="my-3"><i class="bi bi-envelope" style="color:#ff5821;"></i>
-                        <a style="color:#000" href="mailto:info@capeezacorp.com"> info@capeezacorp.com</a></b><br>
-                    </p>
-                    <div class="social-links text-start text-lg-left pt-3 mt-4 pt-lg-0">
-                        <!-- <a href="#" class="twitter" target=""><i class="bx bxl-twitter"></i></a> -->
-                        <a href="https://www.facebook.com/Capeeza-Corp-Services-113344597510715" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
-                        <a href="https://www.instagram.com/capeezacorpservices/" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
-                        <a href="#" class="google-plus" target=""><i class="bx bxl-google"></i></a>
-                        <!-- <a href="#" class="linkedin" target=""><i class="bx bxl-linkedin"></i></a> -->
-                    </div>
-                </div>
-
-                <div class="col-lg-4 col-md-6 footer-links">
-                    <h4>Useful Links</h4>
-                    <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://incometaxindia.gov.in/Pages/default.aspx">Income Tax Department</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="http://cbec.gov.in/">Central Board Of Excise & Custom</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://www.tin-nsdl.com/">E-Tax Information Network</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://www.mca.gov.in/content/mca/global/en/home.html">Ministry Of Company Affairs</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://www.epfindia.gov.in/site_en/">Employees Provident fund</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://www.gst.gov.in/">GST</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://maharera.mahaonline.gov.in/">RERA</a></li>
-
-                    </ul>
-                </div>
-
-                <div class="col-lg-4 col-md-6 footer-links">
-                    <h4>Connect With Us</h4>
-
-                    <ul>
-                        <li><b>Manchar</b></li>
-                        <li><i class='bx bx-map'></i> <a href="#" target="">Trimurti pride, Below Bank of Maharashtra, Manchar,
-                                Pune - 410 503</a></li>
-                        <li><b>Pune</b></li>
-                        <li><i class='bx bx-map'></i> <a href="#" target="">Kunal Icon Road, Pimple Saudagar, Pune - 411 027</a>
-                        </li>
-                        <li><b>Navi Mumbai</b></li>
-                        <li><i class='bx bx-map'></i> <a href="#" target="">03, Sai Villa, Plot No. 75, Sector - 20, Ulwe, Navi
-                                Mumbai - 410 206</a></li>
-                        <li><i class='bx bx-map'></i> <a href="https://goo.gl/maps/U61TZtp9DdT6ADbPA" target="_blank">Office No. 69, Thapar Complex, Plot No. 51, Sec - 15,
-                                CBD Belapur, Navi Mumbai - 400 614.</a></li>
-
-                    </ul>
-                    <!-- <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-                    </ul> -->
-                </div>
-
-                <!-- <div class="col-lg-4 col-md-6 footer-newsletter">
-                    <h4>Join Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-                    <form action="" method="post">
-                        <input type="email" name="email"><input type="submit" value="Subscribe">
-                    </form>
-                </div> -->
-
-            </div>
-        </div>
-    </div>
-    <div class="container py-4">
+      <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 copyright pt-1 copyright_footer" style="text-align:left;">
-                © 2022<a href="#" target="_blank"><b> Capeeza Corp Services</b></a>| All
-                Rights Reserved
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 copyright_footer"  style="text-align:right;">
-                Designed by <a href="https://tectignis.in" target="_blank"><b>Tectignis IT Solutions.</b></a>
-            </div>
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>Company</h3>
+            <p>
+              We are located at Pune and<br> Navi Mumbai providing a wide<br>  range of services on PAN India<br> basis and across the Globe. <br><br>
+              <strong>Phone:</strong>  +91 9371151356<br>
+              <strong>Email:</strong>info@capeezacorp.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Income Tax Department</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Central Board Of Excise & Custom</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                E-Tax Information Network</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">
+                Ministry Of Company Affairs</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Employees Provident fund</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">GST</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">RERA</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Connect With Us</h4>
+            <ul>
+              <h6>Manchar</h6>
+              <li><i class="bi bi-geo-alt"></i> <a href="#">
+                Trimurti pride, Below Bank of Maharashtra, Manchar, Pune - 410 503</a></li>
+                <h6>Pune</h6>
+                <li><i class="bi bi-geo-alt"></i> <a href="#">
+                  Kunal Icon Road, Pimple Saudagar, Pune - 411 027</a></li>
+                  <h6>Navi Mumbai</h6>
+                  <li><i class="bi bi-geo-alt"></i> <a href="#">
+                    03, Sai Villa, Plot No. 75, Sector - 20, Ulwe, Navi Mumbai - 410 206</a></li>
+                   
+                    <li><i class="bi bi-geo-alt"></i> <a href="#">
+                      Office No. 69, Thapar Complex, Plot No. 51, Sec - 15, CBD Belapur, Navi Mumbai - 400 614.</a></li>
+              
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
 
-    <!-- <div class="container d-lg-flex py-4">
+    <div class="container d-md-flex py-4">
 
-      
-        <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
-            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>2022 Capeeza Corp Services|</span></strong>. All Rights Reserved
         </div>
-    </div> -->
-</footer><!-- End Footer -->
-
-
-
-
-
-
+        
+      </div>
+      <div class="credits" style="float: right;">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+        Designed by <a href="https://tectignis.in/"> Tectignis IT Solutions.</a>
+      </div>
+    </div>
+    
+  </footer><!-- End Footer -->
--- include/header.php
+++ include/header.php
@@ -2,9 +2,9 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.php"><span>Com</span>pany</a></h1>
+      <!-- <h1 class="logo me-auto"><a href="index.php"><span>Com</span>pany</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.php" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      <a href="index.php" class="logo me-auto"><img src="assets/img/logo-capeeza.webp" alt="" class="img-fluid"></a>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
@@ -28,30 +28,30 @@
               <li class="dropdown"><a href="#"><span>Finance</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
                  
-                                <li><a href="accounting.php" class="Acc">Working &amp; Capital</a></li>
-                                <li><a href="Tax-Advisory.php" class="TaxAdv">Project Finance</a></li>
-                                <li><a href="Business-Advisory.php" class="BusiAdv">Unsecured Business Loan</a></li>
-                                <li><a href="Business-Setup.php" class="BusiSet">Lap</a></li>
-                                <li><a href="Virtual-CFO.php" class="Virtual">Home Loan</a></li>
+                                <li><a href="Working-Capital.php" class="Acc">Working &amp; Capital</a></li>
+                                <li><a href="Project-Finance.php" class="TaxAdv">Project Finance</a></li>
+                                <li><a href="Unsecured-Business-Loan.php" class="BusiAdv">Unsecured Business Loan</a></li>
+                                <li><a href="LAP.php" class="BusiSet">Lap</a></li>
+                                <li><a href="Home-Loan.php" class="Virtual">Home Loan</a></li>
                 </ul>
               </li>
               <li class="dropdown"><a href="#"><span>Compliance</span> <i class="bi bi-chevron-right"></i></a>
                 <ul>
                  
-                                <li><a href="accounting.php" class="Acc">Ministry Of Coprporate Affairs(MCA)</a></li>
-                                <li><a href="Tax-Advisory.php" class="TaxAdv">SEBI Compliance</a></li>
-                                <li><a href="Business-Advisory.php" class="BusiAdv">ROC Compliance</a></li>
-                                <li><a href="Business-Setup.php" class="BusiSet">RBI & FEMA Compliance</a></li>
+                                <li><a href="MCA.php" class="Acc">Ministry Of Coprporate Affairs(MCA)</a></li>
+                                <li><a href="SEBI-Compliance.php" class="TaxAdv">SEBI Compliance</a></li>
+                                <li><a href="ROC-Compliance.php" class="BusiAdv">ROC Compliance</a></li>
+                                <li><a href="RBI-FEMA-Cmpliance.php" class="BusiSet">RBI & FEMA Compliance</a></li>
                 </ul>
               </li>
-              <li><a href="about.php">EXIM</a></li>
-              <li><a href="about.php">About Us</a></li>
+              <li><a href="EXIM.php">EXIM</a></li>
+              <li><a href="Registration.php">Registration</a></li>
             </ul>
           </li>
 
-          <li><a href="services.php">Industries We Serve</a></li>
+          <!-- <li><a href="services.php">Industries We Serve</a></li> -->
           <li><a href="portfolio.php">Portfolio</a></li>
-          <li><a href="pricing.php">Career</a></li>
+          <!-- <li><a href="pricing.php">Career</a></li> -->
           <li><a href="contact.php">Contact Us</a></li>
 
         </ul>
