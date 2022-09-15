a98107235f4106411536f97c307ec49e92ae2c6c
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-09-15 14:11:26 +0530

    sss

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bi.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/bx.css" rel="stylesheet">
+
+  <link href="assets/vendor/boxicons/css/bxIcon.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/ri.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
