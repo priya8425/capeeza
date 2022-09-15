c9f4d263b4d8d088c9509b4c56c3945b0c05630b
priya8425 <yadavpriya1425@gmail.com>
2022-09-15 09:26:35 +0530

    header

---
+++ include/header.php
@@ -0,0 +1,69 @@
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html"><span>Com</span>pany</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="about.html">About Us</a></li>
+
+          <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+             
+              <li class="dropdown"><a href="#"><span>Corporate</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                 
+                                <li><a href="accounting.php" class="Acc">Accounting &amp; Auditing</a></li>
+                                <li><a href="Tax-Advisory.php" class="TaxAdv">Tax Advisory</a></li>
+                                <li><a href="Business-Advisory.php" class="BusiAdv">Business Advisory</a></li>
+                                <li><a href="Business-Setup.php" class="BusiSet">Business Setup</a></li>
+                                <li><a href="Virtual-CFO.php" class="Virtual">Virtual SFO Services</a></li>
+                                <li><a href="International-Taxation.php" class="IntTax">International Taxation</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Finance</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                 
+                                <li><a href="accounting.php" class="Acc">Working &amp; Capital</a></li>
+                                <li><a href="Tax-Advisory.php" class="TaxAdv">Project Finance</a></li>
+                                <li><a href="Business-Advisory.php" class="BusiAdv">Unsecured Business Loan</a></li>
+                                <li><a href="Business-Setup.php" class="BusiSet">Lap</a></li>
+                                <li><a href="Virtual-CFO.php" class="Virtual">Home Loan</a></li>
+                </ul>
+              </li>
+              <li class="dropdown"><a href="#"><span>Compliance</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                 
+                                <li><a href="accounting.php" class="Acc">Ministry Of Coprporate Affairs(MCA)</a></li>
+                                <li><a href="Tax-Advisory.php" class="TaxAdv">SEBI Compliance</a></li>
+                                <li><a href="Business-Advisory.php" class="BusiAdv">ROC Compliance</a></li>
+                                <li><a href="Business-Setup.php" class="BusiSet">RBI & FEMA Compliance</a></li>
+                </ul>
+              </li>
+              <li><a href="about.html">EXIM</a></li>
+              <li><a href="about.html">About Us</a></li>
+            </ul>
+          </li>
+
+          <li><a href="services.html">Industries We Serve</a></li>
+          <li><a href="portfolio.html">Portfolio</a></li>
+          <li><a href="pricing.html">Career</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
