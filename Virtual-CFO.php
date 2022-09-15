1aa23926a79a6ca88613cbab37c8112512af868d
priya8425 <yadavpriya1425@gmail.com>
2022-09-15 09:53:46 +0530

    footer include

--- Virtual-CFO.php
+++ Virtual-CFO.php
@@ -139,77 +139,9 @@
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
-  <footer id="footer">
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6 footer-contact">
-            <h3>Company</h3>
-            <p>
-              We are located at Pune and<br> Navi Mumbai providing a wide<br>  range of services on PAN India<br> basis and across the Globe. <br><br>
-              <strong>Phone:</strong>  +91 9371151356<br>
-              <strong>Email:</strong>info@capeezacorp.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">
-                Income Tax Department</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">
-                Central Board Of Excise & Custom</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">
-                E-Tax Information Network</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">
-                Ministry Of Company Affairs</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Employees Provident fund</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">GST</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">RERA</a></li>
-            </ul>
-          </div>
-          <div class="col-lg-4 col-md-6 footer-links">
-            <h4>Connect With Us</h4>
-            <ul>
-              <h6>Manchar</h6>
-              <li><i class="bi bi-geo-alt"></i> <a href="#">
-                Trimurti pride, Below Bank of Maharashtra, Manchar, Pune - 410 503</a></li>
-                <h6>Pune</h6>
-                <li><i class="bi bi-geo-alt"></i> <a href="#">
-                  Kunal Icon Road, Pimple Saudagar, Pune - 411 027</a></li>
-                  <h6>Navi Mumbai</h6>
-                  <li><i class="bi bi-geo-alt"></i> <a href="#">
-                    03, Sai Villa, Plot No. 75, Sector - 20, Ulwe, Navi Mumbai - 410 206</a></li>
-                   
-                    <li><i class="bi bi-geo-alt"></i> <a href="#">
-                      Office No. 69, Thapar Complex, Plot No. 51, Sec - 15, CBD Belapur, Navi Mumbai - 400 614.</a></li>
-              
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>2022 Capeeza Corp Services|</span></strong>. All Rights Reserved
-        </div>
-        
-      </div>
-      <div class="credits" style="float: right;">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
-        Designed by <a href="https://tectignis.in/"> Tectignis IT Solutions.</a>
-      </div>
-    </div>
-    
-  </footer><!-- End Footer -->
+  <?php 
+include('include/footer.php');
+?>
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
